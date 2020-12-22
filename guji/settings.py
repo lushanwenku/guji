526cd4f373732ccbe1ee3cd826f8c69dd13051a5
770679957@qq.com <00002121yyw>
2020-12-23 04:20:04 +0800

    初始化

--- guji/settings.py
+++ guji/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'guji.spiders'
 #USER_AGENT = 'guji (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- guji/spiders/newdu_spider.py
+++ guji/spiders/newdu_spider.py
@@ -1,15 +1,29 @@
 import scrapy
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-
-settings = get_project_settings()
-crawler = CrawlerProcess(settings)
-
 
 class NewduSpider(scrapy.Spider):
     name = 'newdu'
     allowed_domains = ['newdu.com']
     start_urls = ['http://ab.newdu.com/book/']
+    newdu_url = 'http://ab.newdu.com/book/'
 
     def parse(self, response):
+        # 获取分类名
+        cate_nam_list = response.xpath('//*[@id="book_right"]/div[1]/div[2]/div[*]/dl/dt/a/text()').extract()
+        cate_url_list = response.xpath('//*[@id="book_right"]/div[1]/div[2]/div[*]/dl/dt/a/@href').extract()
+        print(cate_nam_list)
+        print(cate_url_list)
+        # for samll_cate_url in cate_url_list:
+        #     print(samll_cate_url)
+        #     yield scrapy.Request(self.newdu+newdu_url,callback=self.parse_next)
         pass
+
+    def parse_next(self,response):
+        small_cate_name_list = response.xpath('//*[@id="book_right"]/div[2]/div[2]/div[*]/div[2]/h1/a[1]/text()').extract()
+        small_cate_name_url_list = response.xpath('//*[@id="book_right"]/div[2]/div[2]/div[*]/div[2]/h1/a[1]/@href').extract()
+        book_name_list = response.xpath('//*[@id="book_right"]/div[2]/div[2]/div[*]/div[2]/h1/a[2]/text()').extract()
+        book_name_url_list = response.xpath('//*[@id="book_right"]/div[2]/div[2]/div[*]/div[2]/h1/a[2]/@href').extract()
+        book_author_list = response.xpath('//*[@id="book_right"]/div[2]/div[2]/div[*]/div[2]/h2').extract()
+        print(small_cate_name_url_list)
+        print(book_name_url_list)
+        item = GujiItem()
+        yield item
